f8d1f291a7cce76fb6ec34155148bae2f6e41d47
yodahe1 <yodahemeless@gmail.com>
2024-12-31 14:54:08 +0300

    mongodb is conected and server is running

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+async function main() {
+    await mongoose.connect(process.env.MONGO);
+
+    
+}
+main().then(()=>console.log("mongodb is connected")).catch(err => console.log(err));
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
